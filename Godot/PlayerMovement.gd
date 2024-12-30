be4caa690900da46490f70608f4b1074ea1354d9
Luigi573 <106448901+Luigi573@users.noreply.github.com>
2024-12-29 22:49:03 -0600

    Create tilemaps

--- Godot/PlayerMovement.gd
+++ Godot/PlayerMovement.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 var inverted_sprite = false
 
 @export var speed = 300.0
-@export var jump_height = -800.0
+@export var jump_height = -500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
